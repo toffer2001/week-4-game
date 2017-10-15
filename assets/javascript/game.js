32464e7d83cac54ce0913b6a699388649772ae6c
Kristoffer Keene <Kristoffer Keene>
2017-10-14 21:14:26 -0600

    final as best as could

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-	var character = {
+	var characters = {
 		"Yoda": {
 			name: "Yoda",
 			health: 120,
@@ -38,20 +38,34 @@ $(document).ready(function() {
 		}
 	};
 
+	console.log(characters);
+
 	var cursorSelectedCharacter;
 	var combatants = [];
+	var currDefender;
+	var turnCounter = 1;
+	var killCount = 0;
 
-	console.log(character);
-
-	var renderOne = function(character, renderArea) {
+	var renderOne = function(character, renderArea, charStatus) {
 		var charDiv = $("<div class='character' data-name='" + character.name + "'>");
 		var charName = $("<div class='character-name'>").text(character.name);
 		var charImage = $("<img alt='image' class='character.image'>").attr("src", character.imageUrl);
 		var charHealth = $("<div class='character-health'>").text(character.health);
 		charDiv.append(charName).append(charImage).append(charHealth);
 		$(renderArea).append(charDiv);
+		console.log("character name: " , character.name);
 
-	}
+		//if character is an enemy or defender
+		if (charStatus === "enemy") {
+			$("charDiv").addClass("enemy");
+		}
+		else if (charStatus === "defend") {
+			currDefender = character;
+			console.log(currDefender);
+			$(charDiv).addClass("target-enemy");
+		}
+
+	};
 
 	var displayCharacters = function(charObj, areaRender) {
 		if (areaRender === "#character-section") {
@@ -59,45 +73,144 @@ $(document).ready(function() {
 
 			for (var key in charObj) {
 				if(charObj.hasOwnProperty(key)) {
-						renderOne(charObj[key], areaRender);
+					renderOne(charObj[key], areaRender, "");
 				}
 			}
 		}
-	}
+	
+		if (areaRender === "#selected-character") {			
+			renderOne(charObj, areaRender, "");
+		
+		}
 
-		// if (areaRender === "#selected-character") {
-		// 	renderOne(charObj, areaRender);
-		// }
+		if (areaRender === "#enemies-to-attack") {
+			for(var i=0; i<charObj.length; i++) {
+				renderOne(charObj[i], areaRender, "enemy");
+			}
 
-		// if (areaRender === "#enemies-to-attack") {
-		// 	for(var i=0; i<charObj.length; i++) {
-		// 		renderOne(charObj[i], areaRender);
-		// 	}
-		// }
 
-	// displayCharacters(character, "#character-section");
+			$(document).on("click", ".enemy", function() {
+			console.log("this clicked 2");
+				var name = $(this).attr("data-name");
+				
+					if($("#defend").children().length ===0) {
+						displayCharacters(name,"#defend");
+						$(this).hide();
+					}
+			
+			});
 
-	$(document).on("click", ".fighters", function() {
-		console.log("this clicked");
-	// 	var name = $(this).attr("data-name");
-	// 	console.log(name);
+		}
+
+		
+		//Display "active opponent"
+		if (areaRender === "#defend") {
+			$(areaRender).empty();
+				for(var i=0; i<combatants.length; i++) {
+					if(combatants[i].name ===charObj) {
+						renderOne(combatants[i], areaRender, "defend");
+					}
+				}
+		}
+
+		//Re-display defender when attacked.
+		if (areaRender === "playerDamage") {
+			$("#defend").empty();
+			renderOne(charObj, "#defend", "defend");
+			
+		}
+
+		//Re-render player when attacked.
+		if (areaRender === "enemyDamage") {
+			$("selected-character").empty();
+			renderOne(charObj, "#selected-character", "");
+		}
+
+		//Remove defeated enemy.
+		if (areaRender === "enemyDefeated") {
+			$("#defend").empty();
+		}
+
+
+	};
+
+	var restartGame = function (inputEndGame) {
+		var restart = $("<button>Restart</button>").click(function () {
+			location.reload();
+		});
+
+		var gameState = $("<div>").text(inputEndGame);
+
+		$("body").append(gameState);
+		$("body").append(restart);
+	};
+
+	// Display characters when game starts
+	displayCharacters(characters, "#character-section");
+
+	//Select character
+	$(document).on("click", ".character", function() {
+		var name = $(this).attr("data-name");
 
 	// If a player character has not yet been chosen
 	if (!cursorSelectedCharacter) {
-		for (var key in character) {
+		cursorSelectedCharacter = characters[name];
+		for (var key in characters) {
 			if (key !== name) {
-				combatants.push(character[key]);
+				combatants.push(characters[key]);
 			}
+		// else {cursorSelectedCharacter = characters[key]};
+
+		$("#pick").hide();
 		}
 
-		console.log(combatants);
+		
+		console.log("combatants: " + combatants);
 
 		// Hide character select div.
 		$("#character-section").hide();
 
-		// Display character and combatants.
+
 		displayCharacters(cursorSelectedCharacter, "#selected-character");
 		displayCharacters(combatants, "#enemies-to-attack");
-	}
-	})
+
+		}
+	});
+
+	$("#attack-button").on("click"), function() {
+			if($("#defend").children().length !==0) {
+				currDefender.health -= (cursorSelectedCharacter.attack = turnCounter);
+
+				if (currDefender.health > 0) {
+
+					// Reduce enemy's updated character card
+					displayCharacters(currDefender,"playerDamage");
+
+					// Reduce your health by opponent's attack value
+					cursorSelectedCharacter.health -+ currDefender.enemyAttackBack;
+
+					// Render the player's updated character card.
+					displayCharacters(cursorSelectedCharacter, "enemyDamage");
+
+					if(cursorSelectedCharacter.health <= 0) {
+						renderMessage("clearMessage");
+						restartGame("You have been defeated.");
+						$("#attack-button").unbind("click");
+					}
+				}
+				else {
+			//Remove opponent's character card.
+					displayCharacters(currDefender, "enemyDefeated");
+					killCount++;
+					if (killCount >= 3) {
+						renderMessage("clearMessage");
+						restartGame("You won!");
+					}
+				}
+		}
+
+		turnCounter++;
+	
+
+	};
 });
